fe35e36907b7f644afd41453c68e17816e3aa969
tfoucault <thibault.foucault@gmail.com>
2014-10-02 00:09:56 +0200

    Ajout du dossier dist au repository

---
+++ dist/_locales/en/errors.js
@@ -0,0 +1,12 @@
+var ERRORS_EN = {
+    NO_CONTENT_TO_TRANSLATE: 'There is no selected word to translate.',
+    SOURCE_LANGUAGE_UNDEFINED: 'Source language isn undefined.',
+    TARGET_LANGUAGE_UNDEFINED: 'Target language is undefined',
+    LANGUAGE_ISO_UNDEFINED: 'Undefined language iso code.',
+    API_BASE_URL_UNDEFINED: 'Base url for api call is undefined',
+    API_VERB_UNDEFINED: 'Method to join on API is undefined',
+    API_PARAMS_UNDEFINED: 'Params to call API method are undefined',
+    API_KEY_UNDEFINED: 'The account key for Microsoft Translate API is undefined',
+    USER_LANGUAGE_UNDEFINED: 'User language is undefined',
+    NO_CONTENT_TO_DETECT: 'No content provided for detecting source language'
+};
